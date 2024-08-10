91831950c9ba9a4bc08f642dad4136ddbf16ff37
whx <528457592@qq.com>
2024-08-10 20:27:29 +0800

    first commit

---
+++ hellow/entry/src/main/ets/pages/Index.js
@@ -0,0 +1,10 @@
+import InfoTop from '../components/info';
+import InfoItem from '../components/infoitem';
+struct Index extends   {
+    constructor() { }
+    build() {
+            .width('100%')
+            .height('100%');
+    }
+}
+//# sourceMappingURL=Index.js.map
